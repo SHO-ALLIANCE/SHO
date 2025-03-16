8d5ac5fd3d2afc81ddca77f1e7fccbd4071aa743
SHO-ALLIANCE <sho.strategic.heroes.oracles@gmail.com>
2025-03-16 17:57:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Show loader when the page loads
+window.onload = function () {
+    setTimeout(() => {
+        document.getElementById("loader").style.opacity = "0";
+        setTimeout(() => {
+            document.getElementById("loader").style.display = "none";
+        }, 500);
+    }, 1000);
+};
+
+// Show loader when clicking links
+document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function (event) {
+        if (this.getAttribute("href") !== "#" && this.getAttribute("target") !== "_blank") {
+            event.preventDefault();
+            document.getElementById("loader").style.display = "flex";
+            setTimeout(() => {
+                window.location.href = this.getAttribute("href");
+            }, 1000);
+        }
+    });
+});
